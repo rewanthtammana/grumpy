72cfbeef8741d02976bff7d891aeaa5c1f0a73e4
testinguser883 <testinguser883@gmail.com>
2021-12-06 10:56:15 +0400

    Adds notary validation support

--- grumpy.go
+++ grumpy.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
 	"io/ioutil"
@@ -32,7 +33,7 @@ func (gs *GrumpyServerHandler) serve(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "empty body", http.StatusBadRequest)
 		return
 	}
-	glog.Info("Received request")
+	// glog.Info("Received request")
 
 	if r.URL.Path != "/validate" {
 		glog.Error("no validate")
@@ -48,23 +49,23 @@ func (gs *GrumpyServerHandler) serve(w http.ResponseWriter, r *http.Request) {
 
 	raw := arRequest.Request.Object.Raw
 	admissionRequestUID := arRequest.Request.UID
-	glog.Info("admissionRequestUID")
-	glog.Info(admissionRequestUID)
+	glog.Infof("admissionRequestUID = %s\n", admissionRequestUID)
+	// glog.Info(admissionRequestUID)
 	
 	pod := v1.Pod{}
 	if err := json.Unmarshal(raw, &pod); err != nil {
 		glog.Error("error deserializing pod")
 		return
 	}
-	podName := fmt.Sprintf("Pod name = %s\n", pod.Name)
-	glog.Info("podName")
-	glog.Info(podName)
-	podInfo := fmt.Sprintf(">>>> Podx image = %v\n", pod)
-	glog.Info("PodInfo")
-	glog.Info(podInfo)
-	podUID := fmt.Sprintf("Pod name = %s\n", pod.UID)
-	glog.Info("PodUID")
-	glog.Info(podUID)
+	// podName := fmt.Sprintf("Pod name = %s\n", pod.Name)
+	// glog.Info("podName")
+	// glog.Info(podName)
+	// podInfo := fmt.Sprintf(">>>> Podx image = %v\n", pod)
+	// glog.Info("PodInfo")
+	// glog.Info(podInfo)
+	// podUID := fmt.Sprintf("Pod name = %s\n", pod.UID)
+	// glog.Info("PodUID")
+	// glog.Info(podUID)
 
 	// var containerName string
 	app := "./notary-slim"
@@ -75,30 +76,110 @@ func (gs *GrumpyServerHandler) serve(w http.ResponseWriter, r *http.Request) {
 	for i := 0; i < len(pod.Spec.Containers); i++ {
 		imageName := pod.Spec.Containers[i].Image
 
-		glog.Infof("===> Pod name = %s ; Container name = %s\n", podName, imageName)
+		// glog.Infof("===> Pod name = %s ; Container name = %s\n", podName, imageName)
 
 		// arg2 := os.Getenv("GUN")
 		// arg3 := os.Getenv("TARGET")
 		gun := strings.Split(imageName, ":")[0]
 		target := strings.Split(imageName, ":")[1]
+		r, err := regexp.Compile(target + " sha256:[a-f0-9]+")
+		if err != nil {
+			glog.Errorf("Regex compile error = %v\n", err)
+			arResponse := v1beta1.AdmissionReview{
+				Response: &v1beta1.AdmissionResponse{
+					UID: admissionRequestUID,
+					Allowed: false,
+					Result: &metav1.Status{
+						Message: "Regex compile error",
+					},
+				},
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "admission.k8s.io/v1",
+					Kind:       "AdmissionReview",
+				},
+			}
+		
+			resp, err := json.Marshal(arResponse)
+		
+			if err != nil {
+				glog.Errorf("Can't encode response: %v", err)
+				http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+			}
+			if _, err := w.Write(resp); err != nil {
+				glog.Errorf("Can't write response: %v", err)
+				http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+			}
+			// return
+		}
 
 		cmd := exec.Command(app, subcommand, arg0, arg1, gun, target)
 		stdout, err := cmd.Output()
 
 		if err != nil {
-			glog.Errorf("Notary error = %v\n", err)
-			return
+			glog.Errorf("No signing information found for = %s\nError message: %v", imageName, err)
+			arResponse := v1beta1.AdmissionReview{
+				Response: &v1beta1.AdmissionResponse{
+					UID: admissionRequestUID,
+					Allowed: false,
+					Result: &metav1.Status{
+						Message: "No signing information found for " + imageName,
+					},
+				},
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "admission.k8s.io/v1",
+					Kind:       "AdmissionReview",
+				},
+			}
+		
+			resp, err := json.Marshal(arResponse)
+		
+			if err != nil {
+				glog.Errorf("Can't encode response: %v", err)
+				http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+			}
+			if _, err := w.Write(resp); err != nil {
+				glog.Errorf("Can't write response: %v", err)
+				http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+			}
+			// return
 		}
 
 		// Print the output
-		glog.Infof("--- Notary output for %s\n", imageName)
+		// glog.Infof("--- Notary output for %s\n", imageName)
+		m := r.FindStringIndex(string(stdout))
+		if len(m) == 0 {
+			glog.Errorf("No matching signature found for %v\n", imageName)
+			arResponse := v1beta1.AdmissionReview{
+				Response: &v1beta1.AdmissionResponse{
+					UID: admissionRequestUID,
+					Allowed: false,
+					Result: &metav1.Status{
+						Message: "Length = 0; No signing information found for " + imageName,					},
+				},
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "admission.k8s.io/v1",
+					Kind:       "AdmissionReview",
+				},
+			}
+		
+			resp, err := json.Marshal(arResponse)
+		
+			if err != nil {
+				glog.Errorf("Can't encode response: %v", err)
+				http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+			}
+			if _, err := w.Write(resp); err != nil {
+				glog.Errorf("Can't write response: %v", err)
+				http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+			}
+		}
 		glog.Infof("+++ %v\n", string(stdout))
-		fmt.Println(string(stdout))
+		// fmt.Println(string(stdout))
 	}
 
-	if pod.Name == "smooth-app" {
-		return
-	}
+	// if pod.Name == "smooth-app" {
+	// 	return
+	// }
 
 	arResponse := v1beta1.AdmissionReview{
 		Response: &v1beta1.AdmissionResponse{
@@ -114,36 +195,14 @@ func (gs *GrumpyServerHandler) serve(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	arResponseDebug := fmt.Sprintf(">>>> Podx image = %v\n", arResponse)
-	glog.Info("arResponseDebug")
-	glog.Info(arResponseDebug)
-
 	resp, err := json.Marshal(arResponse)
-	
-	// type respArr struct {
-	// 	ApiVersion string
-	// 	Kind string
-	// 	Response *v1beta1.AdmissionReview
-	// }
-	// respUpdate := &respArr{
-	// 	ApiVersion: "admission.k8s.io/v1",
-	// 	Kind: "AdmissionReview",
-	// 	Response: &arResponse,
-	// }
-	// respUpdateJson, _ := json.Marshal(respUpdate)
 
 	if err != nil {
 		glog.Errorf("Can't encode response: %v", err)
 		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
 	}
-	glog.Info("resp")
-	glog.Info(string(resp))
-	glog.Infof("Ready to write reponse ...")
 	if _, err := w.Write(resp); err != nil {
-	// if _, err := w.Write(respUpdateJson); err != nil {
 		glog.Errorf("Can't write response: %v", err)
 		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
 	}
-	glog.Info("resp-end")
-	glog.Info(string(resp))
 }
