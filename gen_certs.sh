59efc03a1d436d94c7fcdf99e226519d72d4a491
Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>
2022-01-05 16:30:25 +0000

    Kubernetes v1.22 fix validationwebhook

--- gen_certs.sh
+++ gen_certs.sh
@@ -1,24 +1,21 @@
-#!/bin/bash
+keydir="certs"
+cd "$keydir"
 
-set -o errexit
-set -o nounset
-set -o pipefail
+openssl genrsa -out ca.key 2048
+openssl req -x509 -new -nodes -key ca.key -days 100000 -out ca.crt -subj "/CN=admission_ca"
+openssl genrsa -out warden.key 2048
+openssl req -new -key warden.key -out warden.csr -subj "/CN=grumpy.default.svc" -config ../server.conf
+openssl x509 -req -in warden.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out warden.crt -days 100000 -extensions v3_req -extfile ../server.conf
 
-# CREATE THE PRIVATE KEY FOR OUR CUSTOM CA
-openssl genrsa -out certs/ca.key 2048
+# cp warden.crt cert.pem
+# cp warden.key key.pem
 
-# GENERATE A CA CERT WITH THE PRIVATE KEY
-openssl req -new -x509 -key certs/ca.key -out certs/ca.crt -config certs/ca_config.txt
+cp warden.crt grumpy-crt.pem
+cp warden.key grumpy-key.pem
 
-# CREATE THE PRIVATE KEY FOR OUR GRUMPY SERVER
-openssl genrsa -out certs/grumpy-key.pem 2048
-
-# CREATE A CSR FROM THE CONFIGURATION FILE AND OUR PRIVATE KEY
-openssl req -new -key certs/grumpy-key.pem -subj "/CN=grumpy.default.svc" -out grumpy.csr -config certs/grumpy_config.txt
-
-# CREATE THE CERT SIGNING THE CSR WITH THE CA CREATED BEFORE
-openssl x509 -req -in grumpy.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/grumpy-crt.pem
+keydir="../"
+cd "$keydir"
 
 # INJECT CA IN THE WEBHOOK CONFIGURATION
-export CA_BUNDLE=$(cat certs/ca.crt | base64 | tr -d '\n')
+export CA_BUNDLE=$(cat certs/grumpy-crt.pem | base64 | tr -d '\n')
 cat _manifest_.yaml | envsubst > manifest.yaml
